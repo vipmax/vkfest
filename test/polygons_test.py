7874e08e5fc9dc2e7d2643c6deea33017429c618
vipmax <djvipmax@gmail.com>
2016-07-15 19:15:13 +0300

    fixed polygons

--- test/polygons_test.py
+++ test/polygons_test.py
@@ -4,21 +4,23 @@ node_re = re.compile('id="(\w+)"')
 lat_re = re.compile('lat="(\w+.\w+)"')
 lon_re = re.compile('lon="(\w+.\w+)"')
 nodes_and_points = {}
-with open("/home/maxpetrov/PycharmProjects/vkfest1/data/vkfest_points.osm", "r") as f:
+with open("/home/maxpetrov/PycharmProjects/vkfest1/data/vkfest_final_zones.osm", "r") as f:
     for line in f.readlines():
         # print(line)
-        node_id = node_re.findall(line)[0]
-        lat = lat_re.findall(line)[0]
-        lon = lon_re.findall(line)[0]
-        # print(node_id, lat, lon)
-        nodes_and_points[int(node_id)] = (lat, lon)
+        try:
+            node_id = node_re.findall(line)[0]
+            lat = lat_re.findall(line)[0]
+            lon = lon_re.findall(line)[0]
+            # print(node_id, lat, lon)
+            nodes_and_points[int(node_id)] = (lat, lon)
+        except:
+            pass
+
 
 
 fnode_re = re.compile('ref="(\w+)"')
 fest_re = re.compile('v="vkfest (\w+)"')
-
-
-with open("/home/maxpetrov/PycharmProjects/vkfest1/data/vkfest.osm", "r") as f:
+with open("/home/maxpetrov/PycharmProjects/vkfest1/data/vkfest_final_zones.osm", "r") as f:
     for line in f.readlines():
         # print(line)
         try:
